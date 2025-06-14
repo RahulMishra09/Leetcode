acffea0b02aa594c90b14a81b7ba8db6e2332ad2
Rahul Mishra <115176525+RahulMishra09@users.noreply.github.com>
2025-06-14 08:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (67.33%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,21 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return pow(x, (long) n); // Convert to long to handle Integer.MIN_VALUE
-    }
-
-    private double pow(double x, long n) {
-        if (n == 0) {
-            return 1.0;
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
 
-        if (n < 0) {
-            return 1.0 / pow(x, -n);
+        double result = 1.0;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
         }
-
-        if (n % 2 == 1) { // Odd exponent
-            return x * pow(x * x, (n - 1) / 2);
-        } else { // Even exponent
-            return pow(x * x, n / 2);
-        }
+        return result;
     }
 }
