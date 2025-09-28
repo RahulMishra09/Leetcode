930c1e8d021b963bd28e53e5e4a58e1e155c15a2
Rahul Mishra <115176525+RahulMishra09@users.noreply.github.com>
2025-09-29 02:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (92.49%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,17 +1,21 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> num= new Stack<>();
-        int ans=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='('){
-                num.push(ans);
-                ans=0;
-            }else{
-                ans=num.pop()+Math.max(2*ans,1);
+        int depth = 0;
+        int score = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                depth++;
+            } else {
+                depth--;
+                if (s.charAt(i - 1) == '(') {
+                    score += 1 << depth; 
+                }
             }
         }
-        return ans;
-        
+
+        return score;
     }
 }
