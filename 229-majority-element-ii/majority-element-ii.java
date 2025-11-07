8581cbda99e0296d4fa2cc6b82c80668c09d98bd
Rahul Mishra <115176525+RahulMishra09@users.noreply.github.com>
2025-11-07 07:38:35 +0530

    Time: 15 ms (25.07%) | Memory: 53 MB (5.02%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,25 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        HashMap<Integer,Integer> store=new HashMap<>();
+        for (int num : nums) {
+            store.put(num, store.getOrDefault(num, 0) + 1);
+        }
         int n = nums.length;
-        HashMap<Integer, Integer> store = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
-            if (store.containsKey(nums[i])) {
-                store.put(nums[i], store.get(nums[i]) + 1);
-            } else {
-                store.put(nums[i], 1);
-            }
-        }
-
-        Set<Integer> keyset = store.keySet();
-        List<Integer> majorityElements = new ArrayList<>();
-        
-        for (int key : keyset) {
+        for (int key : store.keySet()) {
             if (store.get(key) > n / 3) {
-                majorityElements.add(key);
+                ans.add(key);
             }
         }
-
-        return majorityElements;       
+        return ans;
+    
     }
 }
